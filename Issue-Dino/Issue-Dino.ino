45b8c835fdaa483b93ca7adaab2ce21f3748e560
avsavushkiny <avsavushkiny@live.ru>
2022-02-07 20:23:47 +0300

    Create Issue-Dino.ino

---
+++ Issue-Dino/Issue-Dino.ino
@@ -0,0 +1,35 @@
+/*
+
+Hello!
+
+issue Dino
+
+Alexander Savushkin
+05.02.22
+
+*/
+
+void setup()
+{
+
+}
+
+void loop()
+{
+    startGame();
+}
+
+void startGame()
+{
+
+}
+
+void startPage()
+{
+
+}
+
+void overPage()
+{
+
+}
