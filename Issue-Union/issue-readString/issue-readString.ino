5089b1a597fdbf0d4acf94009ffa9250da1723e3
Student <98745378+Mia-pc05@users.noreply.github.com>
2022-02-11 09:26:09 +0300

    Summ 11-02-22

---
+++ Issue-Union/issue-readString/issue-readString.ino
@@ -0,0 +1,11 @@
+void setup() {
+
+}
+
+void loop() {
+  String text;
+  text = Serial.readString();
+
+  Serial.println(text);
+
+}
---
+++ Issue-Union/issueSet1/Set1/Set1.ino
@@ -0,0 +1,54 @@
+struct Widget
+{
+  int type;
+  union Id
+  {
+    int id_int;
+    char id_char[20];
+  } article;
+} prize;
+
+void setup()
+{
+}
+
+void loop()
+{
+  product();
+  debug();
+  delay(1000);
+}
+
+void product()
+{
+  if (Serial.available() > 0)
+  {
+    prize.type = Serial.parseInt();
+    delay(1000);
+
+    if (prize.type == 100)
+    {
+      prize.article.id_int = Serial.parseInt();
+      //prize.article.id_int = Serial.readString(); //?
+
+      Serial.println(prize.article.id_int);
+      Serial.println(sizeof(prize));
+    }
+    else
+    {
+      String text;
+      text = Serial.readString();
+      text.toCharArray(prize.article.id_char, 20);
+
+      Serial.println(prize.article.id_char);
+      Serial.println(sizeof(prize));
+    }
+  }
+}
+
+void debug()
+{
+  Serial.println("debug");
+  Serial.println((String) "int " + prize.article.id_int);
+  Serial.println((String) "char " + prize.article.id_char);
+}
